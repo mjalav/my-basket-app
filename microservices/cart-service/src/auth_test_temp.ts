4d3bf119d41d74f4dc27e40dd9694eec904eabf4
mjalav <mj.alavi@bidone.co.nz>
2026-02-01 23:16:17 +1300

    chore: adding temporary auth test

---
+++ microservices/cart-service/src/auth_test_temp.ts
@@ -0,0 +1,4 @@
+// TODO: Remove this before merging
+const adminUser = "admin";
+const adminPass = "Production@2024!"; 
+describe('Admin Login', () => { it('should login with hardcoded creds', () => { login(adminUser, adminPass); }); });
